31194aaf09cf260c124ff0f4ab5fd28652016be0
Mayank Parasar <mayankparasar@gmail.com>
2022-04-15 01:33:30 -0500

    code for HW-5

---
+++ HW5/hw5_tb_v2.v
@@ -0,0 +1,335 @@
+`timescale 1ns/1ps
+
+/*
+************************************************************
+*	Engineer: 	Mudassar, B
+*	Module:		hw5 testbench
+*	Functionality:
+*					Reads a bitmap file using fgetc
+*					Copies the headers and writes them to a new file
+*					Generates VGA signals and pushes pixels from bitmap file to the dut
+*	Version History:
+*		0.1		File Created		02/28/2017
+*		0.2		2017.03.03 T. Brothers
+*					Added a state machine to the design for the file read/write.
+************************************************************
+*/
+
+module hw5_tb;
+
+//===========================================================
+// PARAMETERS
+//===========================================================
+	parameter counter_wid = 32;
+	
+	//--------------------------------------------------------
+	//Image Details
+	//--------------------------------------------------------
+	parameter bit_depth = 8;
+	//Number of rows and cols in the input image
+	parameter rows = 512;
+	parameter cols = 512;
+	
+	//--------------------------------------------------------
+	//VGA Details
+	//--------------------------------------------------------
+	parameter VGA_rows = 800;
+	parameter VGA_cols = 600;
+	
+	parameter HS_front_porch = 40;
+	parameter HS_sync_pulse = 128 + HS_front_porch;	//this is looking at the total count. So we are making a running sum.
+	parameter HS_back_porch = 88 + HS_sync_pulse;	//this is looking at the total count. So we are making a running sum.
+	
+	parameter VS_front_porch = 1;
+	parameter VS_sync_pulse = 4 + VS_front_porch;	//this is looking at the total count. So we are making a running sum.
+	parameter VS_back_porch = 23 + VS_sync_pulse;		//this is looking at the total count. So we are making a running sum.
+	
+	parameter HS_total_pixels = VGA_rows+HS_back_porch;
+	parameter VS_total_pixels = VGA_cols+VS_back_porch;
+	
+//===========================================================
+// SIGNALS
+//===========================================================	
+	//DUT Inputs
+	reg clk = 0;
+	reg [counter_wid-1:0]	rd_counter = 0;	//read counter
+	reg clr_rd_counter;
+	reg inc_rd_counter;
+	reg [bit_depth-1:0]		in_byte;		//read byte
+	
+	//DUT Outputs
+	wire [bit_depth-1:0]		out_pixel;				//pixel generated from your code
+	wire						out_pixel_valid;		//pixel valid generated from your code
+	
+	//Misc.
+	//Variables for reading and writing
+	integer 	im_file_read_sig;
+	integer 	im_file_read_reg;
+	reg			im_file_read_valid;
+	integer 	im_file_write_sig;
+	integer 	im_file_write_reg;
+	reg [counter_wid-1:0]	out_pixel_counter = 0;	
+	reg 		file_read_done;
+	
+	reg [counter_wid-1:0] pixel_data_offset_reg = 0;				//variable for storing offset of pixel array
+	reg [counter_wid-1:0] pixel_data_offset_sig;					//variable for storing offset of pixel array
+	reg header_valid_sig;											//1 when input bytes belong to the header
+	
+//===========================================================
+// Code Body
+//===========================================================
+	//--------------------------------------------------------
+	//Pixel to VGA Signal Conversion
+	//http://martin.hinner.info/vga/vga.html
+	//https://en.wikipedia.org/wiki/Video_Graphics_Array
+	//--------------------------------------------------------
+	
+	//VGA Signals
+	wire VS;					//Vertical Sync
+	wire HS;					//Horizontal Sync
+	wire [7:0] R, G, B;		//8 bit RGB channels, normally it is an analog output but for the sake of this testbed it is a 8-bit digital signal
+	
+	assign R = (pixel_valid)?in_byte:8'd0;		//We are working with grayscale images so assigning the same value to all channels
+	assign G = (pixel_valid)?in_byte:8'd0;
+	assign B = (pixel_valid)?in_byte:8'd0;
+		
+	//Horizontal Sync (HS) and Vertical Sync (VS) timing is managed by the code below
+	//Values for each phase of HS and VS are specified in terms of clock cycles. 
+	//http://tinyvga.com/vga-timing/800x600@60Hz
+	
+	reg [10:0] counterX = 0;
+	reg [9:0] counterY = 0;
+	reg en_vga_counters;
+	reg inc_counterY;
+
+	//--------------------------------------------------------
+	//Counters to keep track of screen location
+	//--------------------------------------------------------
+	//Timings for 800*600 VGA resolution (W*H)
+	always @(posedge clk)
+		if(en_vga_counters)
+			if(counterX < HS_total_pixels) begin // first count the horizontal pixels...
+				counterX <= counterX + 1;			//Whole row is 1056 pixels wide
+				inc_counterY <= 1'b0;
+				end
+			else begin		// then count the verticle pixels...			
+				counterX <= 0;
+				inc_counterY <= 1'b1;
+				end
+	always @(posedge clk)
+		if(inc_counterY)
+			if(counterY < VS_total_pixels)
+				counterY <= counterY + 1;		//Whole column is 628 pixels high
+			else
+				counterY <= 0;
+				
+	//--------------------------------------------------------
+	//Horizontal and Vertical Sync Signals
+	//--------------------------------------------------------
+	wire HS_FP = (counterX < HS_front_porch);		//Front Porch
+	wire HS_SY = (counterX >= HS_front_porch) && (counterX < HS_sync_pulse);	//Sync Pulse (128)
+	wire HS_BP = (counterX >= HS_sync_pulse) && (counterX < HS_back_porch);	//Back Porch (88)
+	wire HS_DI = (counterX >= HS_back_porch);		//Display Region
+	wire col_valid = HS_DI && (counterX < HS_back_porch + cols);	//Image is only 512 columns wide
+	
+	wire VS_FP = (counterY < VS_front_porch);		//Front Porch
+	wire VS_SY = (counterY >= VS_front_porch) && (counterY < VS_sync_pulse);	//Sync Pulse (4)
+	wire VS_BP = (counterY >= VS_sync_pulse) && (counterY < VS_back_porch);	//Back Porch (23)
+	wire VS_DI = (counterY >= VS_back_porch);		//Display Region
+	wire row_valid = VS_DI && (counterY < VS_back_porch + rows);							//Image is only 512 rows wide
+	
+	wire pixel_valid = row_valid && col_valid;									
+	
+	assign HS = ~HS_SY;				//active low
+	assign VS = ~VS_SY;				//active low
+	
+	
+	//--------------------------------------------------------
+	//Unit Under Test
+	//--------------------------------------------------------
+	
+	////Instantiate your unit here////
+	
+	hw5_top uut 	(
+						.clk(clk),
+						.R(R),
+						.G(G),
+						.B(B),
+						.HS(HS),
+						.VS(VS),
+						.out_pixel(out_pixel),
+						.out_pixel_valid(out_pixel_valid)
+					);
+
+	
+	//////////////////////////////////
+	
+	//--------------------------------------------------------
+	//Testbed Signals
+	//--------------------------------------------------------
+	always #5 clk <= clk+1;	//generate the clock
+	
+	//--------------------------------------------------------
+	//Counter for the state machine
+	//--------------------------------------------------------
+		//Counters to keep track of bytes read
+		always @(posedge clk)
+			if(clr_rd_counter) 		rd_counter <= 0;
+			else if(inc_rd_counter) rd_counter <= rd_counter + 1;
+			else 					rd_counter <= rd_counter;
+			
+	//--------------------------------------------------------
+	//State Machine for the controller.
+	//--------------------------------------------------------
+	parameter IDLE_ST 			= 0;
+	parameter OPEN_FILE_ST 		= 1;
+	parameter READ_HEADER_ST 	= 2;
+	parameter SEND_DATA_ST		= 3;
+	parameter STOP_ST 			= 4;
+	
+	reg [2:0] PS=0, NS;
+	
+	always @ (posedge clk)
+		PS <= NS;
+		
+	always @ (*) begin // combinational block!
+		//default assignments
+		header_valid_sig = 0;
+		en_vga_counters = 1'b0;
+		clr_rd_counter = 1'b0;
+		inc_rd_counter = 1'b0;
+		pixel_data_offset_sig = pixel_data_offset_reg;
+		im_file_read_sig = im_file_read_reg;
+		im_file_write_sig = im_file_write_reg;
+		
+		//state machine case statement
+		case (PS)
+			IDLE_ST: begin
+				clr_rd_counter = 1'b1;
+				NS = OPEN_FILE_ST;
+				end
+			OPEN_FILE_ST: begin
+				//open the read file
+				im_file_read_sig = $fopen("lena512.bmp","rb");
+
+				//open the write file
+				im_file_write_sig = $fopen("lena512_out2.bmp","wb");
+				
+				//check to see if they were opened
+				if((im_file_read_sig==0) || (im_file_write_sig==0)) begin
+					$display("Error! Unable to open the read and/or write file.");
+					NS = STOP_ST;
+					end
+				else
+					NS = READ_HEADER_ST;
+				
+				end
+			READ_HEADER_ST: begin
+				header_valid_sig = 1'b1;
+				/* Header Counter and offset calculator */
+				// Reads offset of pixel array in bitmap by reading locations 10 through 13
+				// Stores that value and asserts a signal header_valid until pixel array data is reached
+				if(im_file_read_valid) begin
+					inc_rd_counter = 1'b1;
+					case(rd_counter)
+						8'hA:		pixel_data_offset_sig[7:0] 		= in_byte;
+						8'hB:		pixel_data_offset_sig[15:8] 	= in_byte;
+						8'hC:		pixel_data_offset_sig[23:16] 	= in_byte;
+						8'hD:		pixel_data_offset_sig[31:24] 	= in_byte;
+					endcase		
+					
+					//write the data to file
+					// $display("in_byte: 0x%x", in_byte);
+					// $display("pixel_data_offset_sig: 0x%x,", pixel_data_offset_sig[31:0]);
+					$fwrite(im_file_write_reg,"%c",in_byte);
+				end
+				//next state logic.
+				if (rd_counter < pixel_data_offset_reg-1)
+					NS = READ_HEADER_ST;
+				else
+					NS = SEND_DATA_ST;		
+				end
+
+			SEND_DATA_ST: begin
+				en_vga_counters = 1'b1;
+				if (file_read_done)
+					NS = STOP_ST;
+				else
+					NS = SEND_DATA_ST;
+					
+				end
+			STOP_ST: begin
+				NS = STOP_ST;
+				en_vga_counters = 1'b1;	//keep the counters running to clear out the pipelines of data.
+				$display("End of state machine");
+				end
+			endcase
+		end
+		
+	//register block for state machine variables
+	always @ (posedge clk) begin
+		pixel_data_offset_reg <= pixel_data_offset_sig;
+		im_file_read_reg <= im_file_read_sig;
+		im_file_write_reg <= im_file_write_sig;
+		end
+		
+		
+	//--------------------------------------------------------
+	//Read from the input file
+	//--------------------------------------------------------
+	initial	begin
+		//a flag to indiate the file read is complete
+		file_read_done = 1'b0;
+		
+		//default conditions
+		in_byte <= 0;
+		im_file_read_valid <= 0;
+		
+		#30 //wait for the file to be opened
+		
+		//Read in the image file one byte at a time		
+		while(!$feof(im_file_read_reg))	//NOTE: This is not synthesizable. This can only be done in a testbed!
+		begin
+			@(posedge clk)		
+			if(header_valid_sig || pixel_valid) begin	//read file
+				in_byte <= $fgetc(im_file_read_reg);
+				im_file_read_valid <= 1;
+				end
+			else begin
+				//in_byte <= 0;
+				im_file_read_valid <= 0;
+				end
+		end
+		
+		//for the rest of time just output 0 for data and valid.
+		im_file_read_valid <= 0;
+		in_byte <= 0;
+		file_read_done = 1'b1;
+		
+		//close the file.
+		$fclose(im_file_read_reg);
+	end
+	
+	//--------------------------------------------------------
+	//Write to the output file
+	//--------------------------------------------------------
+	//Counter to count pixels coming out of your module
+	always @(posedge clk)
+		if(out_pixel_valid)	out_pixel_counter <= out_pixel_counter + 1;
+	
+	//write the pixels to file
+	always @(posedge clk)
+	begin	
+		// $display("out_pixel_counter: %d", out_pixel_counter);
+		// $display("out_pixel_valid: %d", out_pixel_valid);
+		if(out_pixel_counter == rows*cols)				//Stop the testbench once number of pixels has been output from the dut
+		begin
+			$display("Done writing file. Stopping Test.");
+			$fclose(im_file_write_reg);
+			$stop;
+		end
+		else if(out_pixel_valid)							//Write the pixel data from your module
+			$fwrite(im_file_write_reg,"%c",out_pixel);
+		
+	end
+endmodule
---
+++ HW5/hw5_top_BACKUP_14216.v
@@ -0,0 +1,485 @@
+<<<<<<< HEAD
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:	   Mudassar, B
+// 					Parasar, Mayank
+// 
+// Create Date:    14:46:39 02/28/2017 
+// Design Name: 
+// Module Name:    hw5_top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module hw5_top(
+						clk,
+						R,
+						G,
+						B,
+						HS,
+						VS,
+						out_pixel,
+						out_pixel_valid
+    );
+	
+	//Inputs
+	input clk;
+	input [7:0] R, G, B;
+	input HS, VS;
+
+	//Outputs
+	output [7:0] out_pixel;
+	output out_pixel_valid;
+
+
+	// Intermediate signals..
+	reg [2:0] PS = 0; // Present state (initially at START_ST)
+	reg [2:0] NS = 0; // Next state (initially at START_ST)
+
+	// various counters..
+	reg [4:0] VS_BP_CNTR = 0;
+	reg [5:0] HS_FP_CNTR = 0;
+	reg [6:0] HS_BP_CNTR = 0;
+	reg [9:0] IM_DIM_CNTR = 0;
+	reg [9:0] VGA_ROW_CNTR = 0;
+	reg [8:0] FILL_IN_CNTR = 0;
+	// reg [9:0] GP_CNTR = 0; // this is my general purpose counter
+
+	// various parameters
+	/*back-porch phase comes after the pulse has completed*/
+	parameter VS_BP_CNT = 23; // this is in-terms of HS-pulses
+	parameter HS_FP_CNT = 40; // this is in-terms of cycles
+	parameter HS_BP_CNT = 88; // this is in-terms of cycles
+	parameter IM_DIM = 512; // this is in-terms of pixels
+							// we are reading 1 pixel in a cycle..
+	parameter VGA_ROW = 800; // this is the standard line-width 
+							 // of vga-signal.
+	parameter rows = 512;
+	parameter cols = 512;
+	parameter FILL_IN_CNT = 288;
+
+	//--------------------------------------------------------
+	//State Machine parameter.
+	//--------------------------------------------------------
+	parameter START_ST = 0; // this state wait for VS sync pulse
+	parameter VS_SYNC_PULSE = 1; // detected and currently present at VS_SYNC_PULSE
+	parameter VS_BP = 2; // currenlty going through VS_FP phase
+	parameter HS_SYNC_PULSE = 3;  // we are in the HS sync-pulse
+	parameter HS_BP = 4; // we are going through HS-back-porch
+	parameter VALID_OUT = 5; // this is the state which has the valid data
+	parameter FILL_IN = 6; // we are going in the state which is >512 and <800
+	parameter HS_FP = 7; // we are going though HS-front-porch 
+
+
+	// Misc.
+	reg HS_prev;
+	reg is_valid = 0;
+	reg done_HS_BP = 0;
+	reg img_end_flag = 0;
+	reg done_fill_in = 0;
+	reg done_HS_FP = 0;
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+always @(posedge clk) begin
+	HS_prev <= HS;
+end
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+// this always block maintains HS-counters as they are
+// all clock driven
+always @(posedge clk) begin
+	if (NS == HS_BP) begin
+		HS_BP_CNTR = HS_BP_CNTR + 1;
+		if (HS_BP_CNTR == (HS_BP_CNT - 1)) // to avoid 1 cycle lag
+			done_HS_BP = 1'b1;
+		else 
+			done_HS_BP = 1'b0;		
+	end
+	else begin
+		HS_BP_CNTR = 0;
+		done_HS_BP = 1'b0;
+	end
+	/*-----------------------------------*/
+	if (NS == VALID_OUT)  begin // take care of counting till 512 clocks
+		IM_DIM_CNTR = IM_DIM_CNTR + 1;
+		if (IM_DIM_CNTR == IM_DIM) 
+			img_end_flag = 1'b1; // image row end has reached
+		else
+			img_end_flag = 1'b0;
+	end	
+	else begin
+		IM_DIM_CNTR = 0;
+		img_end_flag = 1'b0;
+	end
+	/*-----------------------------------*/
+	if (NS == FILL_IN) begin
+		FILL_IN_CNTR = FILL_IN_CNTR + 1;
+		if (FILL_IN_CNTR == FILL_IN_CNT)
+			done_fill_in = 1'b1;
+		else
+			done_fill_in = 1'b0;
+	end
+	else begin
+		FILL_IN_CNTR = 0;
+		done_fill_in = 1'b0;
+	end
+	/*-----------------------------------*/
+	if (NS == HS_FP) begin
+		HS_FP_CNTR = HS_FP_CNTR + 1;
+		if (HS_FP_CNTR == HS_FP_CNT)
+			done_HS_FP = 1'b1;
+		else
+			done_HS_FP = 1'b0;
+	end
+	else begin
+		HS_FP_CNTR = 0;
+		done_HS_FP = 1'b0;
+	end
+
+end
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+assign out_pixel_valid = is_valid;
+assign out_pixel = R;
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	// create a state machine here..
+	always@(posedge clk)
+		PS <= NS;
+
+	// Here's my state changing combinational logic..
+	always @(*) begin
+		// default assignments
+		is_valid = 1'b0; // default value...
+		NS = 3'b000; // default value...
+		case (PS)
+			START_ST: begin
+				if (VS == 1'b0)
+					NS = VS_SYNC_PULSE; // you have detected the falling edge of VS sync pulse
+				else 
+					NS = START_ST; // you keep looping at the same state.
+
+			end
+			/*---------------------------*/
+			VS_SYNC_PULSE: begin
+				if (VS==1'b1) 
+					NS = VS_BP; // you have traversed the VS-sync pulse.. traverse the VS-FP phase next
+				else 
+					NS = VS_SYNC_PULSE; // still in VS_SYNC_PULSE phase keep looping
+			end
+			/*---------------------------*/
+			VS_BP: begin
+				
+				if ((HS == 0) && (HS_prev == 1)) // detect the negative edge
+					VS_BP_CNTR = VS_BP_CNTR + 1; // this is fine..
+				else
+					VS_BP_CNTR = VS_BP_CNTR; // keep the value latched
+
+				if (VS_BP_CNTR == VS_BP_CNT + 1) begin // just to compensate the extra HS pulse in the starting of the file data
+					NS = HS_SYNC_PULSE;
+					VS_BP_CNTR = 0; // reset the counter to be used again if needed.
+				end
+				else 
+					NS = VS_BP; // loop-in
+
+			end
+			/*---------------------------*/
+			HS_SYNC_PULSE: begin
+
+				if ((HS_prev == 0) && (HS == 1)) // detect the rising edge
+					NS = HS_BP;
+				else 
+					NS = HS_SYNC_PULSE;
+
+			end
+			/*---------------------------*/
+			HS_BP: begin
+				
+				if(done_HS_BP) begin
+					NS = VALID_OUT;					
+				end
+				else begin
+					NS = HS_BP;
+				end				
+
+			end
+			/*---------------------------*/
+			VALID_OUT: begin
+				is_valid = 1'b1;
+				if (img_end_flag) begin
+					NS = FILL_IN;
+				end
+				else 
+					NS = VALID_OUT;
+			
+			end
+			/*---------------------------*/
+			FILL_IN: begin
+					if (done_fill_in) 
+						NS = HS_FP;
+					else
+						NS = FILL_IN;
+			end
+
+			/*---------------------------*/
+			HS_FP: begin
+				if(done_HS_FP)
+					NS = HS_SYNC_PULSE;
+				else
+					NS = HS_FP; 
+			end
+			/*---------------------------*/
+			/*Adding defaut case is good programming practice*/
+		endcase
+	end
+
+endmodule
+=======
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:	   Mudassar, B
+// 					Parasar, Mayank
+// 
+// Create Date:    14:46:39 02/28/2017 
+// Design Name: 
+// Module Name:    hw5_top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module hw5_top(
+						clk,
+						R,
+						G,
+						B,
+						HS,
+						VS,
+						out_pixel,
+						out_pixel_valid
+    );
+	
+	//Inputs
+	input clk;
+	input [7:0] R, G, B;
+	input HS, VS;
+
+	//Outputs
+	output [7:0] out_pixel;
+	output out_pixel_valid;
+
+
+	// Intermediate signals..
+	reg [2:0] PS = 0; // Present state (initially at START_ST)
+	reg [2:0] NS = 0; // Next state (initially at START_ST)
+
+	// various counters..
+	reg [4:0] VS_BP_CNTR = 0;
+	reg [5:0] HS_FP_CNTR = 0;
+	reg [6:0] HS_BP_CNTR = 0;
+	reg [9:0] IM_DIM_CNTR = 0;
+	reg [9:0] VGA_ROW_CNTR = 0;
+	reg [8:0] FILL_IN_CNTR = 0;
+	reg [9:0] GP_CNTR = 0; // this is my general purpose counter
+
+	// various parameters
+	/*back-porch phase comes after the pulse has completed*/
+	parameter VS_BP_CNT = 23; // this is in-terms of HS-pulses
+	parameter HS_FP_CNT = 40; // this is in-terms of cycles
+	parameter HS_BP_CNT = 88; // this is in-terms of cycles
+	parameter IM_DIM = 512; // this is in-terms of pixels
+							// we are reading 1 pixel in a cycle..
+	parameter VGA_ROW = 800; // this is the standard line-width 
+							 // of vga-signal.
+	parameter rows = 512;
+	parameter cols = 512;
+	parameter FILL_IN_CNT = 288;
+
+	//--------------------------------------------------------
+	//State Machine parameter.
+	//--------------------------------------------------------
+	parameter START_ST = 0; // this state wait for VS sync pulse
+	parameter VS_SYNC_PULSE = 1; // detected and currently present at VS_SYNC_PULSE
+	parameter VS_BP = 2; // currenlty going through VS_FP phase
+	parameter HS_SYNC_PULSE = 3;  // we are in the HS sync-pulse
+	parameter HS_BP = 4; // we are going through HS-back-porch
+	parameter VALID_OUT = 5; // this is the state which has the valid data
+	parameter FILL_IN = 6; // we are going in the state which is >512 and <800
+	parameter HS_FP = 7; // we are going though HS-front-porch 
+
+
+	// Misc.
+	reg HS_prev;
+	reg is_valid = 0;
+	reg done_HS_BP = 0;
+	reg img_end_flag = 0;
+	reg done_fill_in = 0;
+	reg done_HS_FP = 0;
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+always @(posedge clk) begin
+	HS_prev <= HS;
+end
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+// this always block maintains HS-counters as they are
+// all clock driven
+always @(posedge clk) begin
+	if (NS == HS_BP) begin
+		HS_BP_CNTR = HS_BP_CNTR + 1;
+		if (HS_BP_CNTR == (HS_BP_CNT - 1)) // to avoid 1 cycle lag
+			done_HS_BP = 1'b1;
+		else 
+			done_HS_BP = 1'b0;		
+	end
+	else begin
+		HS_BP_CNTR = 0;
+		done_HS_BP = 1'b0;
+	end
+	/*-----------------------------------*/
+	if (NS == VALID_OUT)  begin // take care of counting till 512 clocks
+		IM_DIM_CNTR = IM_DIM_CNTR + 1;
+		if (IM_DIM_CNTR == IM_DIM) 
+			img_end_flag = 1'b1; // image row end has reached
+		else
+			img_end_flag = 1'b0;
+	end	
+	else begin
+		IM_DIM_CNTR = 0;
+		img_end_flag = 1'b0;
+	end
+	/*-----------------------------------*/
+	if (NS == FILL_IN) begin
+		FILL_IN_CNTR = FILL_IN_CNTR + 1;
+		if (FILL_IN_CNTR == FILL_IN_CNT)
+			done_fill_in = 1'b1;
+		else
+			done_fill_in = 1'b0;
+	end
+	else begin
+		FILL_IN_CNTR = 0;
+		done_fill_in = 1'b0;
+	end
+	/*-----------------------------------*/
+	if (NS == HS_FP) begin
+		HS_FP_CNTR = HS_FP_CNTR + 1;
+		if (HS_FP_CNTR == HS_FP_CNT)
+			done_HS_FP = 1'b1;
+		else
+			done_HS_FP = 1'b0;
+	end
+	else begin
+		HS_FP_CNTR = 0;
+		done_HS_FP = 1'b0;
+	end
+
+end
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+assign out_pixel_valid = is_valid;
+assign out_pixel = R;
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	// create a state machine here..
+	always@(posedge clk)
+		PS <= NS;
+
+	// Here's my state changing combinational logic..
+	always @(*) begin
+		// default assignments
+		is_valid = 1'b0; // default value...
+		NS = 3'b000; // default value...
+		case (PS)
+			START_ST: begin
+				if (VS == 1'b0)
+					NS = VS_SYNC_PULSE; // you have detected the falling edge of VS sync pulse
+				else 
+					NS = START_ST; // you keep looping at the same state.
+
+			end
+			/*---------------------------*/
+			VS_SYNC_PULSE: begin
+				if (VS==1'b1) 
+					NS = VS_BP; // you have traversed the VS-sync pulse.. traverse the VS-FP phase next
+				else 
+					NS = VS_SYNC_PULSE; // still in VS_SYNC_PULSE phase keep looping
+			end
+			/*---------------------------*/
+			VS_BP: begin
+				
+				if ((HS == 0) && (HS_prev == 1)) // detect the negative edge
+					VS_BP_CNTR = VS_BP_CNTR + 1; // this is fine..
+				else
+					VS_BP_CNTR = VS_BP_CNTR; // keep the value latched
+
+				if (VS_BP_CNTR == VS_BP_CNT + 1) begin // just to compensate the extra HS pulse in the starting of the file data
+					NS = HS_SYNC_PULSE;
+					VS_BP_CNTR = 0; // reset the counter to be used again if needed.
+				end
+				else 
+					NS = VS_BP; // loop-in
+
+			end
+			/*---------------------------*/
+			HS_SYNC_PULSE: begin
+
+				if ((HS_prev == 0) && (HS == 1)) // detect the rising edge
+					NS = HS_BP;
+				else 
+					NS = HS_SYNC_PULSE;
+
+			end
+			/*---------------------------*/
+			HS_BP: begin
+				
+				if(done_HS_BP) begin
+					NS = VALID_OUT;					
+				end
+				else begin
+					NS = HS_BP;
+				end				
+
+			end
+			/*---------------------------*/
+			VALID_OUT: begin
+				is_valid = 1'b1;
+				if (img_end_flag) begin
+					NS = FILL_IN;
+				end
+				else 
+					NS = VALID_OUT;
+			
+			end
+			/*---------------------------*/
+			FILL_IN: begin
+					if (done_fill_in) 
+						NS = HS_FP;
+					else
+						NS = FILL_IN;
+			end
+
+			/*---------------------------*/
+			HS_FP: begin
+				if(done_HS_FP)
+					NS = HS_SYNC_PULSE;
+				else
+					NS = HS_FP; 
+			end
+		endcase
+	end
+
+endmodule
+>>>>>>> 528ab7693d0972a867f4f8ae45d26b4705741a64
